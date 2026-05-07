4cd6504859242aaf55c71d0d292b2249b15d3508
Igor-01-Gomes <www.igorgomes@hotmail.com>
2026-05-07 08:31:44 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/pensionat/booking/PensionatBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pensionat.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionatBookingSystemApplication.class, args);
+	}
+
+}
