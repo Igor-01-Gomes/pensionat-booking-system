363d0ccc1d48bdb22ae4d9359d2e8a6ceb608346
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-13 11:18:16 +0200

    Feature/customer service (#11)
    * added CustomerService
    
    * Added CreateCustomerRequest
    
    * Removed unused imports

---
+++ src/main/java/com/pensionat/customer/service/CustomerService.java
@@ -0,0 +1,35 @@
+package com.pensionat.customer.service;
+
+
+import com.pensionat.booking.model.BookingEntity;
+import com.pensionat.booking.repository.BookingRepository;
+import com.pensionat.customer.model.CreateCustomerRequest;
+import com.pensionat.customer.model.CustomerEntity;
+import com.pensionat.customer.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final BookingRepository bookingRepository;
+
+
+    public CustomerService(CustomerRepository customerRepository, BookingRepository bookingRepository) {
+        this.customerRepository = customerRepository;
+        this.bookingRepository = bookingRepository;
+    }
+    public List<CustomerEntity> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+    public CustomerEntity createCustomer(CreateCustomerRequest request) {
+        CustomerEntity customer = new CustomerEntity();
+                customer.setFirstName(request.firstName());
+                customer.setLastName(request.lastName());
+                customer.setEmail(request.email());
+                customer.setHashedPassword(request.hashedPassword());
+                customer.setPhoneNumber(request.phone());
+        return customerRepository.save(customer);
+    }
+}
