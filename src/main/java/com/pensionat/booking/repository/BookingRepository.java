91d42631de5872f8232afeb2d3b6edd7dbece710
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-05-15 10:55:46 +0200

    Features/delete customer (#19)
    * added deleteCustomer method
    
    * added check if customer has an active booking

--- src/main/java/com/pensionat/booking/repository/BookingRepository.java
+++ src/main/java/com/pensionat/booking/repository/BookingRepository.java
@@ -1,8 +1,10 @@
 package com.pensionat.booking.repository;
 
 import com.pensionat.booking.model.BookingEntity;
+import com.pensionat.booking.model.BookingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+    boolean existsByCustomerIdAndBookingStatus(Long customerId, BookingStatus bookingStatus);
 
 }
